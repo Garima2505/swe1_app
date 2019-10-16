e63b57a81a5cc1e0487baf644149792503f9e1f5
Garima Chaudhary <gc2505@nyu.edu>
2019-10-16 08:52:44 -0400

    adding requirements file

--- swe1_app/settings.py
+++ swe1_app/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for swe1_app project.
 
@@ -114,3 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
